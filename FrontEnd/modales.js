5c2035dee362e6849faa988df03969f4438dc2e3
Lucas Pichollet <lucaspichollet@hotmail.com>
2023-03-21 16:19:10 +0100

    ajout bouton ajouter photo

--- FrontEnd/modales.js
+++ FrontEnd/modales.js
@@ -65,6 +65,31 @@ function deleteData(url, tokenKey) {
     return fetch(url, deleteOptions);
 }
 
+        //  d) Fonction d'import d'une nouvelle photo
+
+function importNewPhoto() {
+    const editNewPhoto = document.querySelector('.image-custom');
+    const newPhoto = document.createElement('img');
+    const fileInput = document.querySelector('input[type="file"]');
+    const file = fileInput.files[0];
+
+    const maxPhotoSize = 4 * 1024 * 1024; // ici 4x 1024ko x 1024 octets
+    if (file.size > maxPhotoSize) {
+        alert('Taille de 4go dépassée !');
+        fileInput.value = ''; // permet à l'utilisateur de sélectionner un nouveau fichier
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        newPhoto.src = e.target.result; 
+        editNewPhoto.appendChild(newPhoto);
+    }
+
+    reader.readAsDataURL(file)
+}
+
+
 document.addEventListener("DOMContentLoaded", async function() {
 
         // 2°) Ouverture de la modale en appuyant sur "modifier"
@@ -208,6 +233,59 @@ document.addEventListener("DOMContentLoaded", async function() {
     btnArrow.addEventListener("click", function() {
         hideModeAjout();
         showModePresentation()
+    });
+
+        // 9°) importer une photo
+    
+    const imageCustom = document.querySelector('.image-custom');
+    const btnPhoto = document.querySelector('.btn-photo');
+    const inputFile = document.querySelector('input[type="file"]');
+
+    imageCustom.addEventListener("click", function(event) {
+        event.preventDefault();  
+    });
+
+    btnPhoto.addEventListener("click", function() {
+        inputFile.click();
+    });
+
+    inputFile.addEventListener('change', function() {
+        const imageCustomElement = Array.from(document.querySelectorAll('.image-custom-element'));
+        for (i = 0 ; i < imageCustomElement.length ; i++) {
+            imageCustomElement[i].classList.add("hidden");
+        };
+        importNewPhoto();
+    });
+
+        // 10°) Vérifier si le formulaire est complet avant de valider
+
+    const form = document.getElementById("ajoutPhotoForm");
+
+    function formIsComplete() {
+        const imageInput = form.querySelector("#image");
+        const titleInput = form.querySelector("#title");
+        const categorySelect = form.querySelector('#category-project');
+        const validerButton = document.querySelector('.validerButton');
+    
+        if (imageInput.value.trim() === ''
+            || titleInput.value.trim() === ''
+            || categorySelect.value.trim() === '') {
+            validerButton.style.backgroundColor = "";
+            return false;
+        } else {
+            validerButton.style.backgroundColor = "#1D6154";
+            return true;
+        }
+    }
+
+    form.addEventListener("input", formIsComplete);
+
+    const btnValider = document.querySelector('.validerButton');
+    btnValider.addEventListener("click", function(event) {
+        if (!formIsComplete()) {
+            event.preventDefault();
+            alert("Veuillez remplir tous les champs du formulaire");
+        }
     });
 
 });
